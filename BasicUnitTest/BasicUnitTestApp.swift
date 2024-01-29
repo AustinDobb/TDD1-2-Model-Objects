1a38038e6229e827d7a7b1e2048029de77b18a73
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2024-01-29 08:03:49 -0700

    Initial Commit

---
+++ BasicUnitTest/BasicUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicUnitTestApp.swift
+//  BasicUnitTest
+//
+//  Created by Austin Dobberfuhl on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
